0eda45c8ee259dcdad3d32149b8b04ecc86ade45
Operel <oriperelman@gmail.com>
2020-05-19 10:28:18 +0300

    fix process-existing-document request

--- src/components/FlowManagement/FlowManagementTable.tsx
+++ src/components/FlowManagement/FlowManagementTable.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import './FlowManagementTable.css';
 
-type FlowManagementPropsTypes = {
+type Props = {
   tableName: string,
   documentsList: [],
   headerList: string[],
@@ -11,24 +11,23 @@ type FlowManagementPropsTypes = {
   accessToken?: string
 }
 
-const FlowManagementTable: React.FC<FlowManagementPropsTypes> = ({ tableName, documentsList, headerList, keyValueList, accessToken }) => {
+const FlowManagementTable: React.FC<Props> = ({ tableName, documentsList, headerList, keyValueList, accessToken }) => {
   
-  const processProgressContext = (documentData: any): void => {
+  const processExistingDocument = (documentData: any): void => {
     accessToken = 'Barear ' + accessToken;
-    console.log('Send process-progress-context with documentData: ', documentData, '\n And with accessToken: ', accessToken)
-    fetch(`${process.env.REACT_APP_API_URL}/api/process-progress-context?SessionId=dslf546khds$GgGfhsdfdfsd`, {
+    console.log('Send process-existing-document with documentData: ', documentData, '\n And with accessToken: ', accessToken)
+    fetch(`${process.env.REACT_APP_API_URL}/api/process-existing-document?SessionId=dslf546khds$GgGfhsdfdfsd`, {
       method: 'post',
       headers: {
-        'Content-Type': 'application/json',
-        authorization: accessToken
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(documentData)
     })
     .then((res: any) => res.json())
     .then((data: any) => {
-      console.log('process-progress-context Success Data: ', data);
+      console.log('process-existing-document Success Data: ', data);
     })
-    .catch(err => console.log(err))
+    .catch(err => console.warn('process-existing-document error: ', err))
   }
   
   return (
@@ -43,23 +42,30 @@ const FlowManagementTable: React.FC<FlowManagementPropsTypes> = ({ tableName, do
         </thead>
         <tbody>
           {
-            documentsList ? documentsList.map((doc: any) => (
-              <tr id={doc._id} key={doc._id}>
-                {keyValueList.map((tdKeyValue) => (
-                    <td key={tdKeyValue}>
-                      {typeof eval(tdKeyValue) === 'string' ? eval(tdKeyValue) : JSON.stringify(eval(tdKeyValue)) /** Bad eval! What's the alternative? */}
-                    </td>
-                ))}
-                <td>
-                  {accessToken ?
-                    <Link
-                      to="/user-document"
-                      onClick={() => processProgressContext({ _id: doc._id })}
-                    >Go to document</Link>
-                  : null}
-                </td>
-              </tr>
-            )) : null
+            documentsList ? documentsList.map((doc: any) => {
+              const owner = doc._metadata?.owner;
+              const data = {
+                Owner: owner,
+                DocumentInfo: {selection: {_id: doc._id}, collection: 'documents'}
+              };
+              return (
+                <tr id={doc._id} key={doc._id}>
+                  {keyValueList.map((tdKeyValue) => (
+                      <td key={tdKeyValue}>
+                        {typeof eval(tdKeyValue) === 'string' ? eval(tdKeyValue) : JSON.stringify(eval(tdKeyValue)) /** Bad eval! What's the alternative? */}
+                      </td>
+                  ))}
+                  <td>
+                    {accessToken ?
+                      <Link
+                        to="/user-document"
+                        onClick={() => processExistingDocument(data)}
+                      >Go to document</Link>
+                    : null}
+                  </td>
+                </tr>
+              )
+            }) : null
           }
         </tbody>
       </table>
