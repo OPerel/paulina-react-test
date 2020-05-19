9443400581bbf64ec7128b867366cb969e3b5e41
Operel <oriperelman@gmail.com>
2020-05-19 10:48:49 +0300

    convert UserDocument.tsx to FC with hooks

--- src/components/UserDocument/UserDocument.tsx
+++ src/components/UserDocument/UserDocument.tsx
@@ -1,33 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { withAuth } from '@okta/okta-react';
 
 import UserDocumentTable from './UserDocumentTable/UserDocumentTable';
 
-// import './FlowManagementTable.css';
-
-type FlowManagementPropsTypes = {
+type Props = {
   auth: any
 }
 
-type FlowManagementStateTypes = {
-  userDocument: any
-}
+const UserDocument: React.FC<Props> = ({ auth }) => {
 
-class UserDocument extends Component<FlowManagementPropsTypes, FlowManagementStateTypes> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      userDocument: {}
-    }
-  }
+  const [userDocument, setUserDocument] = useState<any>({})
 
-  accessToken(): string {
-    return this.props.auth.getAccessToken()
+  const getAccessToken = async (): Promise<string> => {
+    return auth.getAccessToken()
       .then((token: string) => token)
   }
 
-  async getUserCurrentContext() {
-    const accessToken = 'Barear ' + await this.accessToken();
+  const getUserCurrentContext = async (): Promise<void> => {
+    const accessToken = 'Barear ' + await getAccessToken();
     console.log('Send get-user-current-context requset with accessToken: ', accessToken);
     fetch(`${process.env.REACT_APP_API_URL}/api/get-user-current-context?SessionId=dslf546khds$GgGfhsdfdfsd`, {
       method: 'post',
@@ -39,24 +29,22 @@ class UserDocument extends Component<FlowManagementPropsTypes, FlowManagementSta
     .then(res => res.json())
     .then((documentData) => {
       console.log('get-user-current-context Success Data: ', documentData);
-      this.setState({ userDocument: documentData })
+      setUserDocument(documentData);
     })
     .catch(err => console.log(err));
   }
 
-  componentDidMount() {
-    this.getUserCurrentContext();
-  }
+  useEffect(() => {
+    getUserCurrentContext();
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <h3>User Document:</h3>
-        {/* <pre style={{ textAlign: 'left' }}>{JSON.stringify(this.state.userDocument, null, 2)}</pre> */}
-        <UserDocumentTable userDocument={this.state.userDocument} />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h3>User Document:</h3>
+      {/* <pre style={{ textAlign: 'left' }}>{JSON.stringify(this.state.userDocument, null, 2)}</pre> */}
+      <UserDocumentTable userDocument={userDocument} />
+    </div>
+  )
 };
 
 export default withAuth(UserDocument);
